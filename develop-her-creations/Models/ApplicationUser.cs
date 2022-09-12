e48e677bef0890f96ae1f4254a7371dcfab5775c
Harley Johnson <62576957+JohnsonHarleyR@users.noreply.github.com>
2022-09-12 15:38:41 -0400

    Add project files.

---
+++ develop-her-creations/Models/ApplicationUser.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace develop_her_creations.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+    }
+}
